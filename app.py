28cc73ece378797d24bbf64f4378ee1fb3c468c4
Fabrício <briciomattoss@gmail.com>
2025-02-10 21:38:09 -0300

    fim do projeto amem

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request 
+from flask import Flask, request, render_template
 import psycopg2, json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Olá, Flask"
+    return render_template('index.html')
 
 @app.route('/item', methods=['POST'])
 def post_item():
@@ -29,6 +29,13 @@ def patch_item(lineNumber):
     banco(sql)
     return data
 
+@app.route('/item/<int:lineNumber>',methods=['DELETE'])
+def delete_item(lineNumber):
+    sql = f"DELETE FROM todolist WHERE \"_lineNumber\" = {lineNumber}"
+    banco(sql)
+    return ""
+    
+
 def banco(sql):
     resultado = ""
     try:
@@ -47,8 +54,6 @@ def banco(sql):
             resultado = cursor.fetchone()[0]
         elif sql[0:6] == "SELECT":
             resultado = cursor.fetchall() # vai guardar o resultado do select na variável resultado
-            colunas = [desc for desc in cursor.description]
-            resultado = colunas
             colunas = [desc[0] for desc in cursor.description]
             resultado = json.dumps([dict(zip(colunas, row)) for row in resultado])
             resultado = json.loads(resultado)
